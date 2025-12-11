b7ade3b48b863c77251e32a7fa469bfc60ee601c
Moustik <81183216+MoustikBe@users.noreply.github.com>
2025-12-11 17:03:51 +0100

    Replacing global variable by structure variable

--- srcs/graphic/texture.py
+++ srcs/graphic/texture.py
@@ -1,30 +1,30 @@
 from main import *
 
 
-def texture_init():
+def texture_init(_game):
     dico_texture = []
     # -- Snake head -- #
     snake_sprite = pygame.image.load("../texture/snake.png").convert()
-    snake_sprite = pygame.transform.scale((snake_sprite), (tile_sprite, tile_sprite))
+    snake_sprite = pygame.transform.scale((snake_sprite), (_game.tile_sprite, _game.tile_sprite))
     dico_texture.append(snake_sprite)
     # -- Snake body -- #
     snake_body_sprite = pygame.image.load("../texture/body.jpg").convert()
-    snake_body_sprite = pygame.transform.scale((snake_body_sprite), (tile_sprite, tile_sprite))
+    snake_body_sprite = pygame.transform.scale((snake_body_sprite), (_game.tile_sprite, _game.tile_sprite))
     dico_texture.append(snake_body_sprite)
     # -- Wall -- # 
     wall_sprite = pygame.image.load("../texture/wall.jpg").convert()
-    wall_sprite = pygame.transform.scale(wall_sprite, (tile_sprite, tile_sprite))
+    wall_sprite = pygame.transform.scale(wall_sprite, (_game.tile_sprite, _game.tile_sprite))
     dico_texture.append(wall_sprite)
     # -- Floor -- #
     floor_sprite = pygame.image.load("../texture/floor.jpg").convert()
-    floor_sprite = pygame.transform.scale(floor_sprite, (tile_sprite, tile_sprite))
+    floor_sprite = pygame.transform.scale(floor_sprite, (_game.tile_sprite, _game.tile_sprite))
     dico_texture.append(floor_sprite)
     # -- R_apple -- #
     r_apple_sprite = pygame.image.load("../texture/r_apple.jpeg").convert()
-    r_apple_sprite = pygame.transform.scale(r_apple_sprite, (tile_sprite, tile_sprite))
+    r_apple_sprite = pygame.transform.scale(r_apple_sprite, (_game.tile_sprite, _game.tile_sprite))
     dico_texture.append(r_apple_sprite)
     # -- G_apple -- #
     g_apple_sprite = pygame.image.load("../texture/g_apple.jpg").convert()
-    g_apple_sprite = pygame.transform.scale(g_apple_sprite, (tile_sprite, tile_sprite))
+    g_apple_sprite = pygame.transform.scale(g_apple_sprite, (_game.tile_sprite, _game.tile_sprite))
     dico_texture.append(g_apple_sprite)
     return (dico_texture)
--- srcs/main.py
+++ srcs/main.py
@@ -3,15 +3,14 @@ import random
 import pygame
 import pdb
 
-tile_sprite = 64
-snake_xpos = 0
-snake_ypos = 0
-snake_len = []
+class MyGame():
+    def __init__(self):
+        self.tile_sprite = 64
+        self.snake_xpos = 0
+        self.snake_ypos = 0
+        self.snake_len = []
 
-def generate_elements(grid, widht, height):
-    global snake_xpos
-    global snake_ypos
-    global snake_len
+def generate_elements(grid, widht, height, _game):
     
     #breakpoint() #Debug
     while(True):
@@ -31,22 +30,22 @@ def generate_elements(grid, widht, height):
         y_snake = random.randint(1, widht - 2)
         if(grid[x_snake][y_snake] == "0"):
             grid[x_snake][y_snake] = "P"
-            snake_xpos = x_snake
-            snake_ypos = y_snake
-            snake_len.append((snake_xpos, snake_ypos))
+            _game.snake_xpos = x_snake
+            _game.snake_ypos = y_snake
+            _game.snake_len.append((_game.snake_xpos, _game.snake_ypos))
             i = 0
             while(i < 2):
                 if(grid[x_snake][y_snake + 1] == '0'):
-                    snake_len.append((x_snake, y_snake + 1))
+                    _game.snake_len.append((x_snake, y_snake + 1))
                     y_snake = y_snake + 1
                 elif(grid[x_snake][y_snake - 1] == '0'):
-                    snake_len.append((x_snake, y_snake - 1))
+                    _game.snake_len.append((x_snake, y_snake - 1))
                     y_snake = y_snake - 1
                 elif(grid[x_snake + 1][y_snake] == '0'):
-                    snake_len.append((x_snake + 1, y_snake))
+                    _game.snake_len.append((x_snake + 1, y_snake))
                     x_snake = x_snake + 1
                 elif(grid[x_snake - 1][y_snake] == '0'):
-                    snake_len.append((x_snake - 1, y_snake))
+                    _game.snake_len.append((x_snake - 1, y_snake))
                     x_snake = x_snake - 1
                 else:
                     print("Error to had snake_len")
@@ -55,7 +54,7 @@ def generate_elements(grid, widht, height):
         break
     return(grid)
 
-def generate_map(widht, height):
+def generate_map(widht, height, _game):
     #breakpoint() #Debug
     grid = []
     for i in range(height):
@@ -66,7 +65,7 @@ def generate_map(widht, height):
             else:
                 row.append("0")
         grid.append(row)
-    return(generate_elements(grid, widht, height))
+    return(generate_elements(grid, widht, height, _game))
 
 
 def new_apple(grid, height, widht, apple):
@@ -78,69 +77,65 @@ def new_apple(grid, height, widht, apple):
             break
     return(grid)
 
-def update_len(grid, _apple):
-    global snake_len
-
+def update_len(grid, _apple, _game):
     if(_apple == 'R'):
-        grid[snake_len[len(snake_len) - 1][0]][snake_len[len(snake_len) - 1][1]] = '0'
-        snake_len.pop()
+        grid[_game.snake_len[len(_game.snake_len) - 1][0]][_game.snake_len[len(_game.snake_len) - 1][1]] = '0'
+        _game.snake_len.pop()
         print("Snake -1 of len")
-        if(len(snake_len) == 1):
+        if(len(_game.snake_len) == 1):
             print("GAME OVER, SNAKE TOO LITLE")
             exit()
         return(grid)
-    x_last = snake_len[len(snake_len) - 1][0] 
-    y_last = snake_len[len(snake_len) - 1][1]
+    x_last = _game.snake_len[len(_game.snake_len) - 1][0] 
+    y_last = _game.snake_len[len(_game.snake_len) - 1][1]
     if(grid[x_last][y_last + 1] == '0'):
-        snake_len.append((x_last, y_last + 1))
+        _game.snake_len.append((x_last, y_last + 1))
     elif(grid[x_last][y_last - 1] == '0'):
-        snake_len.append((x_last, y_last - 1))
+        _game.snake_len.append((x_last, y_last - 1))
     elif(grid[x_last + 1][y_last] == '0'):
-        snake_len.append((x_last + 1, y_last))
+        _game.snake_len.append((x_last + 1, y_last))
     elif(grid[x_last - 1][y_last] == '0'):
-        snake_len.append((x_last - 1, y_last))
+        _game.snake_len.append((x_last - 1, y_last))
     else:
         print("Error to had snake_len")
     print("Snake +1 of len")
     return(grid)
 
-def snake_moov(new_x, new_y, grid):
-    grid[snake_len[len(snake_len) - 1][0]][snake_len[len(snake_len) - 1][1]] = '0'
-    for i in range(len(snake_len) - 1, 0, -1):
-        snake_len[i] = snake_len[i - 1]
-        grid[snake_len[i][0]][snake_len[i][1]] = 'S'
-    snake_len[0] = (new_x, new_y)
+def snake_moov(new_x, new_y, grid, _game):
+    grid[_game.snake_len[len(_game.snake_len) - 1][0]][_game.snake_len[len(_game.snake_len) - 1][1]] = '0'
+    for i in range(len(_game.snake_len) - 1, 0, -1):
+        _game.snake_len[i] = _game.snake_len[i - 1]
+        grid[_game.snake_len[i][0]][_game.snake_len[i][1]] = 'S'
+    _game.snake_len[0] = (new_x, new_y)
     return(grid)
 
-def change_direction(x, y, grid, widht, height):
-    global snake_ypos
-    global snake_xpos
-    if(snake_xpos + x > widht - 2 or snake_xpos + x < 1 or snake_ypos + y > height - 2 or snake_ypos + y < 1):
+def change_direction(x, y, grid, widht, height, _game):
+    if(_game.snake_xpos + x > widht - 2 or _game.snake_xpos + x < 1 or _game.snake_ypos + y > height - 2 or _game.snake_ypos + y < 1):
         print("GAME OVER, SNAKE HIT A WALL")
         exit()
-    if(grid[snake_xpos + x][snake_ypos + y] == 'G'):
+    if(grid[_game.snake_xpos + x][_game.snake_ypos + y] == 'G'):
         grid = new_apple(grid, height, widht, 'G')
-        grid = update_len(grid, 'G')
-    elif(grid[snake_xpos + x][snake_ypos + y] == 'R'):
+        grid = update_len(grid, 'G', _game)
+    elif(grid[_game.snake_xpos + x][_game.snake_ypos + y] == 'R'):
         grid = new_apple(grid, height, widht, 'R')
-        grid = update_len(grid, 'R')
-    grid[snake_xpos][snake_ypos] = '0'
-    if(grid[snake_xpos + x][snake_ypos + y] == 'P' or grid[snake_xpos + x][snake_ypos + y] == 'S'):
+        grid = update_len(grid, 'R', _game)
+    grid[_game.snake_xpos][_game.snake_ypos] = '0'
+    if(grid[_game.snake_xpos + x][_game.snake_ypos + y] == 'P' or grid[_game.snake_xpos + x][_game.snake_ypos + y] == 'S'):
         print("GAME OVER, SNAKE CROSSING ITSELF")
         exit()
-    grid[snake_xpos + x][snake_ypos + y] = 'P'
-    snake_xpos = snake_xpos + x
-    snake_ypos = snake_ypos + y
-    grid = snake_moov(snake_xpos, snake_ypos, grid)
+    grid[_game.snake_xpos + x][_game.snake_ypos + y] = 'P'
+    _game.snake_xpos = _game.snake_xpos + x
+    _game.snake_ypos = _game.snake_ypos + y
+    grid = snake_moov(_game.snake_xpos, _game.snake_ypos, grid, _game)
     return(grid)
 
-def display_map(grid):
-    width = len(grid[0]) * tile_sprite
-    height = len(grid) * tile_sprite
+def display_map(grid, _game):
+    width = len(grid[0]) * _game.tile_sprite
+    height = len(grid) * _game.tile_sprite
 
     pygame.init()
     screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
-    dico_texture = texture.texture_init()
+    dico_texture = texture.texture_init(_game)
     run = True
     while run:
         for event in pygame.event.get():
@@ -148,35 +143,36 @@ def display_map(grid):
                 run = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
-                    grid = change_direction(-1, 0, grid, len(grid[0]), len(grid))
+                    grid = change_direction(-1, 0, grid, len(grid[0]), len(grid), _game)
                 elif event.key == pygame.K_s:
-                    grid = change_direction(+1, 0, grid, len(grid[0]), len(grid))
+                    grid = change_direction(+1, 0, grid, len(grid[0]), len(grid), _game)
                 elif event.key == pygame.K_d:
-                    grid = change_direction(0, +1, grid, len(grid[0]), len(grid))
+                    grid = change_direction(0, +1, grid, len(grid[0]), len(grid), _game)
                 elif event.key == pygame.K_a:
-                    grid = change_direction(0, -1, grid, len(grid[0]), len(grid))
+                    grid = change_direction(0, -1, grid, len(grid[0]), len(grid), _game)
                     
         for i, row in enumerate(grid):
             for j, char in enumerate(row):
                 if char == "1":
-                    screen.blit(dico_texture[2], (j * tile_sprite, i * tile_sprite))
+                    screen.blit(dico_texture[2], (j * _game.tile_sprite, i * _game.tile_sprite))
                 elif char == "P":
-                    screen.blit(dico_texture[0], (j * tile_sprite, i * tile_sprite))
+                    screen.blit(dico_texture[0], (j * _game.tile_sprite, i * _game.tile_sprite))
                 elif char == "S":
-                    screen.blit(dico_texture[1], (j * tile_sprite, i * tile_sprite))
+                    screen.blit(dico_texture[1], (j * _game.tile_sprite, i * _game.tile_sprite))
                 elif char == "0":
-                    screen.blit(dico_texture[3], (j * tile_sprite, i * tile_sprite))
+                    screen.blit(dico_texture[3], (j * _game.tile_sprite, i * _game.tile_sprite))
                 elif char == "R":
-                    screen.blit(dico_texture[4], (j * tile_sprite, i * tile_sprite))
+                    screen.blit(dico_texture[4], (j * _game.tile_sprite, i * _game.tile_sprite))
                 elif char == "G":
-                    screen.blit(dico_texture[5], (j * tile_sprite, i * tile_sprite))
+                    screen.blit(dico_texture[5], (j * _game.tile_sprite, i * _game.tile_sprite))
         pygame.display.flip()
     pygame.quit()
 
 def main():
     print("Main")
-    grid = generate_map(10, 10)
-    display_map(grid)
+    _game = MyGame()
+    grid = generate_map(10, 10, _game)
+    display_map(grid, _game)
     
 
 if(__name__ == "__main__"):
